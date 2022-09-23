0e01e465709b31d4c27257493b7037fa76a352fc
koobonyeon <bonyeun0605@naver.com>
2022-09-24 00:28:36 +0900

    Object 클래스의 메소드 - 깊은 복제(deep clone)

--- src/classes/Member.java
+++ src/classes/Member.java
@@ -1,18 +1,30 @@
 package classes;
 
-public class Member implements Cloneable{
-    public String id;
-    public String name;
-    public String password;
-    public int age;
-    public boolean adult;
+import java.util.Arrays;
 
-    public Member(String id, String name, String password, int age, boolean adult) {
-        this.id = id;
+public class Member implements Cloneable{
+    public String name;
+    public int age;
+    public int[] scores;
+    public Car car;
+
+    public Member(String name, int age, int[] scores, Car car) {
         this.name = name;
-        this.password = password;
         this.age = age;
-        this.adult = adult;
+        this.scores = scores;
+        this.car = car;
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        // 먼저 얕은 복사를 해서 name, age를 복제한다.
+        Member cloned = (Member) super.clone();
+        // scores를 깊은 복제한다.
+        cloned.scores = Arrays.copyOf(this.scores, this.scores.length);
+        // car를 깊은 복제한다.
+        cloned.car = new Car(this.car.model);
+        // 깊은 복제된 Member를 리턴
+        return cloned;
     }
 
     public Member getMember() {
